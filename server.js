3285f9c61d6e72d1b305170665784aa8fa3dda6f
waiying <li.waiying.li@gmail.com>
2018-03-27 17:25:38 -0700

    set up project folder and server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+//Dependencies
+var express = require('express')
+var http = require('http')
+var path = require('path')
+var socketIO = require('socketIO')
+
+var app = express() // basically a function with sig function (request, response) {/.../}
+var server = http.Server(app)
+var io = socketIO(server)
+
+app.set('port', 5000)
+app.use('/static', express.static(__dirname + '/static'))
+
+// Routing
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname, 'index.html'))
+})
+
+// Start the server
+server.listen(5000, function() {
+	console.log('Starting server on port 5000')
+})
